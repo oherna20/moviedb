5c20166e32545f50d9f86d3b507cb70da7622452
oherna20 <orazalhernandez.oh@gmail.com>
2021-05-02 17:08:24 -0500

    Api connection

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,27 +1,25 @@
 <template>
     <div class="flex max-w-full">
-      <img src="https://via.placeholder.com/200x300.png" alt="Movie Poster" />
+        <img
+        :src="`${posterBaseURL}${movie.poster_path}`"
+        :alt="`Poster of ${movie.title}`"
+      />      
       <div
         class="flex flex-col justify-between p-4 leading-normal border-b border-l border-r rounded-b bg-coolGray-100 border-coolGray-400 lg:border-l-0 lg:border-t lg:border-coolGray-400 lg:rounded-b-none lg:rounded-r"
       >
         <div class="mb-8">
-          <div class="mb-2 text-xl font-bold text-coolGray-900">movie.title</div>
-          <p class="text-base text-coolGray-700 line-clamp-6">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
-            repellat non ducimus ipsa autem iste repudiandae, consequatur
-            quibusdam quaerat animi a, placeat reiciendis expedita quos recusandae
-            sequi! Aliquam, sequi ipsum. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Deleniti beatae, velit similique earum voluptatibus
-            ipsam fugit voluptates incidunt necessitatibus voluptatum minus ad
-            quaerat, amet sunt consequuntur perspiciatis praesentium saepe
-            distinctio!
+            <div class="mb-2 text-xl font-bold text-coolGray-900">
+                {{ movie.title }}
+              </div>          
+              <p class="text-base text-coolGray-700 line-clamp-6">
+                {{ movie.overview }}
           </p>
         </div>
         <div class="flex items-start justify-end">
           <div class="flex flex-col items-center">
-            <button class="text-coolGray-400 hover:text-red-600">
-              <mdi:heart class="w-8 h-8" />
-              <span>3</span>
+            <button @click="newLike" class="text-coolGray-400 hover:text-red-600">
+                <mdi:heart class="w-8 h-8" />
+                <span>{{ likes }}</span>
             </button>
           </div>
           <div class="flex flex-col items-center">
@@ -32,10 +30,49 @@
         </div>
       </div>
     </div>
-    <Comments v-if="value" @close="toggle" />
-  </template>
+    <Comments v-if="value" @close="toggle" :movie="movie" />
+</template>
   
   <script setup>
+  import { defineProps, ref, onMounted, toRefs, watch } from 'vue'
     import { useToggle } from '@vueuse/core'
+    
+    import { posterBaseURL } from '~/helpers/useMovies'
+    import { countByObjectId, add } from '~/helpers/useLikes'
+    import { authentication } from '~/helpers/useFirebase'
+    
     const [value, toggle] = useToggle(false)
+
+    const { user } = authentication()
+
+    const props = defineProps({
+    movie: {
+      type: Object,
+      default: () => {
+        return {
+          title: '',
+          overview: '',
+          poster_path: '',
+        }
+      },
+    },
+  })
+
+  const newLike = async () => {
+    await add({ objectId: props.movie.id.toString(), userId: user.value.uid })
+    await getLikes()
+  }
+  const likes = ref(0)
+  const getLikes = async () => {
+    const response = await countByObjectId(props.movie.id.toString())
+    const { data } = response
+    if (data) likes.value = data.count
+  }
+  onMounted(() => {
+    getLikes()
+  })
+  const { movie } = toRefs(props)
+  watch(movie, () => {
+    getLikes()
+  })
   </script>
